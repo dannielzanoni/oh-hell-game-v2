a6a857f97c0a79832f1c106983804ea057f5c74e
Daniel Zanoni <danielzanoni2001@gmail.com>
2025-11-05 00:20:20 -0300

    added zardui and buttons in home component

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -1,8 +1,12 @@
 import { Component } from '@angular/core';
+import { ZardButtonComponent } from '../shared/components/button/button.component';
+import { ZardIconComponent } from '../shared/components/icon/icon.component';
+import { ZardCardComponent } from '@shared/components/card/card.component';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  standalone: true,
+  imports: [ZardButtonComponent, ZardIconComponent, ZardCardComponent],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
